4ed8f314a9c8029232f323e5404a35a8339748b0
avivio <aviv.rotman@gmail.com>
2015-04-24 12:20:46 +0300

    working on entire data set, including umi's to some level, code very messy

--- collect_all_results.py
+++ collect_all_results.py
@@ -1,42 +1,49 @@
 __author__ = 'dell7'
 import os
 import csv
-
+import sys
 
 
 def collect_all_results(final_result_directory):
-    all_result_file_location = final_result_directory + 'all_results.txt'
+    all_result_file_location = final_result_directory + '/all_results.txt'
     all_result_file = open(all_result_file_location,'rb')
     result_files_list = all_result_file.readlines()
-    all_summary_file_location = final_result_directory + 'all_summaries.txt'
-    all_summary_file = open(all_summary_file_location,'wb')
+    all_summary_file_location = final_result_directory + '/all_summaries.txt'
+    all_summary_file = open(all_summary_file_location,'rb')
     summary_files_list = all_summary_file.readlines()
 
 
-    final_result_csv_location = final_result_directory + 'final_result.csv'
+    final_result_csv_location = final_result_directory + '/final_result.csv'
     final_result_csv = csv.writer(open(final_result_csv_location,'wb'))
-    first_result_csv = csv.reader(open(result_files_list[0],'rb'))
+    first_result_csv = csv.reader(open(result_files_list[0].strip(),'rb'))
     result_csvs = []
     for result_file in result_files_list[1:]:
-        result_csvs.append(csv.reader(open(result_file,'rb')))
+        result_csvs.append(csv.reader(open(result_file.strip(),'rb')))
     for line in first_result_csv:
         row = []
         row= line
         for result_csv in result_csvs:
             result_line = result_csv.next()
             row.append(result_line[1])
-            final_result_csv.writerow(row)
-    final_summary_csv_location = final_result_directory + 'final_summary.csv'
+        final_result_csv.writerow(row)
+    final_summary_csv_location = final_result_directory + '/final_summary.csv'
     final_summary_csv = csv.writer(open(final_summary_csv_location,'wb'))
-    first_summary_csv = csv.reader(open(summary_files_list[0],'rb'))
+    first_summary_csv = csv.reader(open(summary_files_list[0].strip(),'rb'))
     summary_csvs = []
     for summary_file in summary_files_list[1:]:
-        summary_csvs.append(csv.reader(open(summary_file,'rb')))
+        summary_csvs.append(csv.reader(open(summary_file.strip(),'rb')))
     for line in first_summary_csv:
         row = []
         row = line
         for summary_csv in summary_csvs:
             summary_line = summary_csv.next()
             row.append(summary_line[1])
-            final_summary_csv.writerow(row)
+        final_summary_csv.writerow(row)
+
+if __name__ == "__main__":
+    #call main giving arguments as so 1-home directory 2-reference file name 3-bin directory 4-result directory
+    # 5- result file 6-discarded trim file 7-discarded variant file 7-summary_file
+
+     collect_all_results(sys.argv[1])
+
 
